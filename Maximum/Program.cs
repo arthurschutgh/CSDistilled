77dcb68f0ff41241929042bac478ca8563bcfb92
Arthur Schut <arthurschutgh@gmail.com>
2020-06-22 16:31:38 +0200

    Added Maximum example

--- Maximum/Program.cs
+++ Maximum/Program.cs
@@ -4,9 +4,36 @@ namespace Maximum
 {
     class Program
     {
+
+        static int Maximum(int a, int b, int c)
+        {
+            if (a > b)
+            {
+                if (a > c)
+                {
+                    return a;
+                }
+                else
+                {
+                    return c;
+                }
+            }
+            else
+            {
+                if (b > c)
+                {
+                    return b;
+                }
+                else
+                {
+                    return c;
+                }
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
+            Console.WriteLine($"Maximum(3, 4, 7) is {Maximum(3, 4, 7)}");
         }
     }
 }
